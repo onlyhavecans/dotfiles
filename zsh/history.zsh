db941557767eb2a86a77a3a7b392f82ec81a988b
tBunnyMan <WagThatTail@Me.com>
2012-07-10 09:47:28 -0700

    ZSH history tweaks

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -11,3 +11,6 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 setopt hist_ignore_space
+
+setopt hist_expire_dups_first
+setopt hist_find_no_dups
