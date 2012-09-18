e6ed321efbaba609f2eac36e9cac5ec88ada4961
tBunnyMan <WagThatTail@Me.com>
2012-09-18 08:00:34 -0700

    AAAAALL sorts of add, an irc plugin and a vim plugin for iaWriter mode

--- work/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias root='TERM=vt220 ssh -Y $1 -l root'
-alias aws='ssh -l root -i ~/.ssh/smcloud.pem $1'
-alias usbserial='screen /dev/tty.PL2303-00002006 9600 8 1'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,8 @@ alias reload!='. ~/.zshrc'
 
 #Good for the iTerm
 alias cleanup="sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /Volumes/*/.Trashes;"
+
+#Work Stuff
+alias root='TERM=vt220 ssh -Y $1 -l root'
+alias aws='ssh -l root -i ~/.ssh/smcloud.pem $1'
+alias usbserial='screen /dev/tty.PL2303-00002006 9600 8 1'
