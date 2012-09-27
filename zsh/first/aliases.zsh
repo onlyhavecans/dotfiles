82c1ff5cd043dd01d831b7f613abd0be4fe04169
tBunnyMan <WagThatTail@Me.com>
2012-09-27 10:25:44 -0700

    Set up remote back to host for urls

--- zsh/first/aliases.zsh
+++ zsh/first/aliases.zsh
@@ -8,6 +8,7 @@ alias cleanup="sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /Volumes/*/
 alias root='TERM=vt220 ssh -Y $1 -l root'
 alias aws='ssh -l root -i ~/.ssh/smcloud.pem $1'
 alias usbserial='screen /dev/tty.PL2303-00002006 9600 8 1'
+alias air='ssh air'
 
 # ZSH Aliases
 alias zshconfig="mate ~/.zshrc"
