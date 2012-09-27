31831393ded70f65f60d69cebb3a288a11462fd1
tBunnyMan <WagThatTail@Me.com>
2012-09-27 10:29:59 -0700

    Cleaned up aliases and added server for talkback

--- zsh/first/aliases.zsh
+++ zsh/first/aliases.zsh
@@ -1,6 +1,3 @@
-alias reload!='. ~/.zshrc'
-alias killscreen="killall ScreensharingAgent"
-
 #Good for the iTerm
 alias cleanup="sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /Volumes/*/.Trashes;"
 
@@ -8,8 +5,13 @@ alias cleanup="sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /Volumes/*/
 alias root='TERM=vt220 ssh -Y $1 -l root'
 alias aws='ssh -l root -i ~/.ssh/smcloud.pem $1'
 alias usbserial='screen /dev/tty.PL2303-00002006 9600 8 1'
+
+#Remote workspace
 alias air='ssh air'
+alias serv='ssh -R 23456:localhost:22 vpn'
+alias killscreen="killall ScreensharingAgent"
 
 # ZSH Aliases
-alias zshconfig="mate ~/.zshrc"
-alias ohmyzsh="mate ~/.oh-my-zsh"
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias reload!='. ~/.zshrc'
