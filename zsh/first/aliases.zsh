3cbd078ad8ceb01d587d0838543f7dcba5906ec9
tBunnyMan <WagThatTail@Me.com>
2012-10-01 14:58:33 -0700

    Update the root command for a better TERM since HPUX is out

--- zsh/first/aliases.zsh
+++ zsh/first/aliases.zsh
@@ -2,8 +2,8 @@
 alias cleanup="sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /Volumes/*/.Trashes;"
 
 #Work Stuff
-alias root='TERM=vt220 ssh -Y $1 -l root'
-alias aws='ssh -l root -i ~/.ssh/smcloud.pem $1'
+alias root='TERM=xterm-256color ssh -Y $1 -l root'
+alias aws='TERM=xterm-256color ssh -l root -i ~/.ssh/smcloud.pem $1'
 alias usbserial='screen /dev/tty.PL2303-00002006 9600 8 1'
 
 #Remote workspace
