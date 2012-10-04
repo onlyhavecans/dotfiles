55e3be721fdd2b98189ce28354dd3f157cf41911
tBunnyMan <WagThatTail@Me.com>
2012-10-03 21:47:39 -0700

    Weechat upgrade

--- zsh/last/editor.zsh
+++ zsh/last/editor.zsh
@@ -1,9 +1,5 @@
 #My Complex editor
-if [[ -f  $HOME/Applications/mvim ]]; then
-	EDITOR="$HOME/Applications/mvim -f"
-elif [[ -f `which mvim` ]]; then
-	EDITOR="`which mvim` -f"
-elif [[ -f /usr/local/bin/vim ]]; then
+if [[ -f /usr/local/bin/vim ]]; then
   EDITOR=/usr/local/bin/vim
 elif [[ -f `which vim` ]]; then
 	EDITOR=`which vim`
