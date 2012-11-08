a7606e081ebfbcda2430d22129f5cd5947f212c6
tBunnyMan <WagThatTail@Me.com>
2012-11-08 14:55:58 -0800

    Weechat alias and postgres path

--- zsh/first/path.zsh
+++ zsh/first/path.zsh
@@ -5,6 +5,11 @@ if [[ -d /Applications ]]; then
 	PATH=/Applications:${PATH}
 fi
 
+#PSQL.app
+if [[ -d /Applications/Postgres.app/Contents/MacOS/bin ]]; then
+  PATH=/Applications/Postgres.app/Contents/MacOS/bin:${PATH}
+fi
+
 # High Priotity /usr/local
 if [[ -d '/usr/local/bin' ]]; then
 	PATH="/usr/local/bin:${PATH}"
@@ -17,11 +22,6 @@ fi
 #User Applications
 if [[ -d ${HOME}/Applications ]]; then
 	PATH=${HOME}/Applications:${PATH}
-fi
-
-# Mac Python 3.2
-if [[ -d '/Library/Frameworks/Python.framework/Versions/3.2/bin' ]]; then
-	PATH="/Library/Frameworks/Python.framework/Versions/3.2/bin:${PATH}"
 fi
 
 export PATH
