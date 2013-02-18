2f308150e845ca5ef0afd65964547367f734a07a
tBunnyMan <WagThatTail@Me.com>
2013-02-17 16:48:04 -0800

    upping vim modules for puppet

--- zsh/first/aliases.zsh
+++ zsh/first/aliases.zsh
@@ -2,7 +2,7 @@
 alias cleanup="/usr/bin/sqlite3 ~/Library/Mail/V2/MailData/Envelope\ Index vacuum; sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /Volumes/*/.Trashes;"
 
 #Work Stuff
-alias root='TERM=vt220 ssh -Y $1 -l root'
+alias root='TERM=vt220 ssh -Y $1 -l root -i ~/.ssh/DHA-SM_rsa'
 alias aws='TERM=xterm-256color ssh -l root -i ~/.ssh/smcloud.pem $1'
 alias usbserial='screen /dev/tty.PL2303-00002006 9600 8 1'
 
