5e8e33dc13992f55804434aedac2dc0f806b3ba4
tBunnyMan <WagThatTail@Me.com>
2013-02-25 12:55:08 -0800

    Exposing more of my style

--- zsh/first/aliases.zsh
+++ zsh/first/aliases.zsh
@@ -2,12 +2,13 @@
 alias cleanup="/usr/bin/sqlite3 ~/Library/Mail/V2/MailData/Envelope\ Index vacuum; sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /Volumes/*/.Trashes;"
 
 #Work Stuff
-alias root='TERM=vt220 ssh -Y $1 -l root -i ~/.ssh/DHA-SM_rsa'
-alias aws='TERM=xterm-256color ssh -l root -i ~/.ssh/smcloud.pem $1'
+alias root='TERM=vt220 ssh -Y $1 -l root -i /Volumes/iamaKey/ssh/DHA-SM_rsa'
+alias aws='TERM=xterm-256color ssh -l root -i /Volumes/iamaKey/ssh/smcloud.pem $1'
 alias usbserial='screen /dev/tty.PL2303-00002006 9600 8 1'
 
 #Remote workspace
-alias serv='mosh --ssh="ssh -p 2222 -i ~/.ssh/Bunni5erv_rsa" vpn'
+alias key="ssh -F /Volumes/iamaKey/ssh/ssh_config"
+alias mish="mosh --ssh='ssh -F /Volumes/iamaKey/ssh/ssh_config'"
 alias killscreen="killall ScreensharingAgent"
 
 # ZSH Aliases
