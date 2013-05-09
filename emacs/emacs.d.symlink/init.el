90dc1de884f82803fcd71fe8287887ae23e114fa
tBunnyMan <WagThatTail@Me.com>
2013-05-09 08:22:39 -0700

    Solarized and Markdown for Emacs, VIM plugin fix

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -1,4 +1,20 @@
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+; Kill these unnecessary modes.
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+; I need themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/solarized")
+(load-theme 'solarized-dark t)
+(set-face-attribute 'default nil :font "PragmataPro-16")
+
+;I use a lot of markdown
+(add-to-list 'load-path "~/.emacs.d/markdown-mode")
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 ; Pi's Demands
 (defun two-pi-r-bind-keys-in-map (mode-map keys-alist)
@@ -30,4 +46,3 @@
      ("\C-z"     . repeat)
      ("\C-x\C-b" . ido-switch-buffer)
      ))
-
