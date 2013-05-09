de895973c9e6d5400fed3c2ccf64ef7bc39d2166
tBunnyMan <WagThatTail@Me.com>
2013-05-09 13:22:10 -0700

    Maybe emacs when I have more time

--- zsh/last/editor.zsh
+++ zsh/last/editor.zsh
@@ -1,8 +1,8 @@
 #My Complex editor
-if [[ -f /usr/local/bin/emacs ]]; then
-  EDITOR=/usr/local/bin/emacs
-elif [[ -f `which emacs` ]]; then
-	EDITOR=`which emacs`
+if [[ -f /usr/local/bin/vim ]]; then
+  EDITOR=/usr/local/bin/vim
+elif [[ -f `which vim` ]]; then
+	EDITOR=`which vim`
 else
 	EDITOR='/bin/vi'
 fi
