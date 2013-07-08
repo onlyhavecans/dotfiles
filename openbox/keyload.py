90988563dea9303e2af7049e1d8336938ac35d1d
tBunnyMan <WagThatTail@Me.com>
2013-07-08 14:51:20 -0700

    I haven't forgotten PEP8

--- openbox/keyload.py
+++ openbox/keyload.py
@@ -8,39 +8,43 @@ mount_point = join(expanduser('~'), 'bunkey')
 keydir = join(mount_point, 'sshkeys')
 keys_re = re.compile(r'(pem|rsa|dsa)$')
 
+
 def get_execute_item(label, command):
     template = Template("""
-	<item label="$label">
-	    <action name="Execute">
-		<execute>$command</execute>
-	    </action>
-	</item>
+    <item label="$label">
+        <action name="Execute">
+            <execute>$command</execute>
+        </action>
+    </item>
     """)
     return template.substitute(label=label, command=command)
 
-def get_seperator(label=None):
+
+def get_separator(label=None):
     if label is None:
-	return "<separator />"
+        return "<separator />"
     else:
-	return "<separator label=\"" + label + "\" />"
+        return "<separator label=\"" + label + "\" />"
+
 
 def get_keys_list(directory, regex):
     files = os.listdir(directory)
     files.sort()
-    return [ f for f in files for m in [regex.search(f)] if m ]
+    return [f for f in files for m in [regex.search(f)] if m]
+
 
 def main():
     if not ismount(mount_point):
-	mount = "/bin/mount " + mount_point
-	print get_execute_item("Mount Drive", mount)
+        mount = "/bin/mount " + mount_point
+        print get_execute_item("Mount Drive", mount)
     else:
-	umount = "/bin/umount " + mount_point
-	print get_execute_item("UnMount Drive", umount)
-	print get_seperator()
+        umount = "/bin/umount " + mount_point
+        print get_execute_item("UnMount Drive", umount)
+        print get_separator()
 
-	for key in get_keys_list(keydir, keys_re):
-	    keycmd = "ssh-add -t 2h " + join(keydir, key)
-	    print get_execute_item(key, keycmd)
+        for key in get_keys_list(keydir, keys_re):
+            keycmd = "ssh-add -t 2h " + join(keydir, key)
+            print get_execute_item(key, keycmd)
 
 
 if __name__ == '__main__':
