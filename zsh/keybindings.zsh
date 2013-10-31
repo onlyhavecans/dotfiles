8f2d50a773f6d86a4d8009dfac87d96ccef68569
David Aronsohn <davidaronsohn@slickdeals.net>
2013-10-31 14:25:09 -0700

    Dear god I need history search

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,3 +1,12 @@
-bindkey '^p' up-line-or-search                      # start typing + [^p] - fuzzy find history forward
-bindkey '^n' down-line-or-search                    # start typing + [^n] - fuzzy find history backward
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 
+# bind P and N for EMACS mode
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
+
+# bind k and j for VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
