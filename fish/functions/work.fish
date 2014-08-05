e830dc96bb566563105c3738bf51c6289f9a1b0b
David Aronsohn <WagThatTail@Me.com>
2014-08-05 11:39:39 -0700

    Update/fix work launcher

--- fish/functions/work.fish
+++ fish/functions/work.fish
@@ -3,8 +3,10 @@ function work
     echo "Oh no! You are already in TMUX"
   else
     if not tmux has-session -t work
-      tmux new-session -d -s work 'vim +NERDTree'
-      tmux split-window -h -l 80 -t work 'irssi'
+      tmux new-session -d -s work
+      #If you call vim directly instead of launching it it breaks tmux/vim
+      tmux send-keys -t work:1.1 'vim +NERDTree' C-m
+      tmux split-window -h -p 30 -t work 'irssi'
       tmux split-window -v -t work
       tmux split-window -v -t work
       tmux select-pane -t work:1.1
