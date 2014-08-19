0afc99b06f4f5ce5dd94245052ea768f82e1be94
David Aronsohn <WagThatTail@Me.com>
2014-08-18 19:03:35 -0700

    add more tabularize pattters and tweak path

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status --is-interactive
-  set PATH /opt/chefdk/bin /opt/chefdk/embedded/bin $HOME/.chefdk/gem/ruby/2.1.0/bin /usr/local/sbin /usr/local/bin $PATH;
+  set PATH /opt/chefdk/bin /opt/chefdk/embedded/bin $HOME/.chefdk/gem/ruby/2.1.0/bin /usr/local/bin /usr/local/sbin $PATH;
   /usr/local/bin/archey --color
 end
 
