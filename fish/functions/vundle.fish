004f3454c624bff02e90be75542115245f460c03
David Aronsohn <WagThatTail@Me.com>
2014-08-19 13:27:56 -0700

    vundle fish shortcut

---
+++ fish/functions/vundle.fish
@@ -0,0 +1,12 @@
+function vundle
+  switch $argv
+    case install
+      vim +PluginInstall +qall
+    case update
+      vim +PluginUpdate +qall
+    case clean
+      vim +PluginClean +qall
+    case '*'
+      echo "No No No No! Try again"
+    end
+  end
