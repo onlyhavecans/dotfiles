4d5790949440efdd33c8ebc535a0d62203cefa82
David Aronsohn <WagThatTail@Me.com>
2014-08-21 12:05:14 -0700

    Clean up gitignores

--- cookbooks/workstation/recipes/default.rb
+++ cookbooks/workstation/recipes/default.rb
@@ -7,18 +7,18 @@
 #
 
 ##
-# Install global packages
-node[:workstation][:packages].each do |my_package|
-  package my_package
-end
-
-##
 # Install all my OS specific goodies
 case node[:platform]
   when 'debian', 'ubuntu'
     include_recipe 'workstation::ubuntu'
   when 'mac_os_x'
     include_recipe 'workstation::mac'
+end
+
+##
+# Install global packages
+node[:workstation][:packages].each do |my_package|
+  package my_package
 end
 
 ##
