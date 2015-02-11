de5955d2622e2e5fc135bc16b0d76d10250aeac2
David Aronsohn <WagThatTail@Me.com>
2015-02-10 22:07:07 -0800

    Merge dotifles into default

--- cookbooks/workstation/metadata.rb
+++ cookbooks/workstation/metadata.rb
@@ -1,9 +1,13 @@
 name             'workstation'
 maintainer       'tBunnyMan'
-maintainer_email ''
+maintainer_email 'WagThatTail@Me.com'
 license          'BSD'
 description      'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version          '0.1.0'
+version          '2.1.0'
+
+%w(mac_os_x mac_os_x_server ubuntu).each do |os|
+  supports os
+end
 
 depends 'homebrew'
--- cookbooks/workstation/recipes/default.rb
+++ cookbooks/workstation/recipes/default.rb
@@ -26,6 +26,46 @@ node['workstation']['packages'].each do |my_package|
 end
 
 ##
-# Dotfile linkup!
-include_recipe 'workstation::dotfiles'
+# Proper dotfile links
+node['workstation']['links'].each do |key, value|
+  original = File.join(workstation_user_home, node['workstation']['dotfiles_dir'], key)
+  dotted = File.join(workstation_user_home, value)
+
+  # Chef can't overwrite folders with symlinks so destroy it if we find one.
+  # This is mostly for fish & vim
+  directory dotted do
+    action :delete
+    recursive true
+    only_if { !File.symlink?(dotted) && File.directory?(dotted) }
+  end
+
+  link dotted do
+    to original
+    user workstation_user
+  end
+end
+
+
+##
+# Setup Vundle
+package 'git'
+
+vundle_dir = File.join(workstation_user_home, ".vim", "bundle", "Vundle.vim")
+
+directory vundle_dir do
+  owner workstation_user
+  recursive true
+end
+
+git vundle_dir do
+  repository node['workstation']['vundle_remote']
+  revision "master"
+  user workstation_user
+  action :checkout
+end
+
+bash 'initalize_vundle' do
+  code 'vim +PluginInstall +qall'
+  user workstation_user
+end
 
