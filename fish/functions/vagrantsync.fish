de0d3e82501f18ab8511d22ef2490c5e663d8016
David Aronsohn <WagThatTail@Me.com>
2015-03-23 17:45:59 -0700

    Vagrant sync

---
+++ fish/functions/vagrantsync.fish
@@ -0,0 +1,9 @@
+function vagrantsync --description 'Syncs the sd-vagrant-dev cookbook with vagrant-dev-vm'
+    cd ~/Code/sd-chef/cookbooks/sd-vagrant-dev
+    berks update
+    berks package ~/Code/vagrant-dev-vm/sd-vagrant-dev.tar
+    cd ~/Code/vagrant-dev-vm/
+    rm -rf cookbooks
+    tar -xvf sd-vagrant-dev.tar
+    rm sd-vagrant-dev.tar
+end
