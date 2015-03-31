369f20d6c80b49c6e390186522f1e891961992ea
David Aronsohn <bitm@slickdeals.net>
2015-03-31 09:55:50 -0700

    Clear up path and add chef settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status --is-interactive
-  set PATH $HOME/Applications /usr/local/bin /usr/local/sbin $PATH;
-  set PATH $PATH /opt/chefdk/bin /opt/chefdk/embedded/bin $HOME/.chefdk/gem/ruby/2.1.0/bin;
+  set PATH /opt/chefdk/bin /opt/chefdk/embedded/bin $HOME/.chefdk/gem/ruby/2.1.0/bin
+  set PATH $PATH $HOME/Applications /usr/local/bin /usr/local/sbin
+  set PATH $PATH /usr/bin /bin /usr/sbin /sbin
   /usr/local/bin/archey --color
 end
 
@@ -10,6 +11,12 @@ end
 
 set -x EDITOR vim
 
+# Chef DK (possibly fragile, damn)
+set -x GEM_ROOT /opt/chefdk/embedded/lib/ruby/gems/2.1.0
+set -x GEM_HOME $HOME/.chefdk/gem/ruby/2.1.0
+set -x GEM_PATH $HOME/.chefdk/gem/ruby/2.1.0:/opt/chefdk/embedded/lib/ruby/gems/2.1.0
+
+# Powerline love
 set -x POWERLINE_ROOT $HOME/dotfiles/vim/bundle/powerline
 set -x POWERLINE_COMMAND $POWERLINE_ROOT/scripts/powerline-render
 set -x POWERLINE_CONFIG_COMMAND $POWERLINE_ROOT/scripts/powerline-config
