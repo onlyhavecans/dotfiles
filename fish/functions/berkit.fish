2af659edd84541aeb0aac3a722b01e72025ca05e
David Aronsohn <WagThatTail@Me.com>
2015-05-21 14:26:31 -0700

    Fix berkit system

--- fish/functions/berkit.fish
+++ fish/functions/berkit.fish
@@ -1,7 +1,9 @@
 function berkit --description "Berks deploy to all sites"
+  echo "~ Running a berks update"
   berks update
   for site in "sdhq" "phx"
-    set -lx $site
+    set -lx chef $site
+    echo "~ Uploading site $site"
     berks upload --no-ssl-verify
   end
 end
