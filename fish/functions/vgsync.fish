7fc931022b3cc0c118a501d0c4de3015a133805c
David Aronsohn <WagThatTail@Me.com>
2015-07-24 19:30:24 -0700

    Clean up snippits

--- fish/functions/vgsync.fish
+++ fish/functions/vgsync.fish
@@ -1,10 +1,6 @@
 function vgsync --description 'Syncs the sd-vagrant-dev cookbook with vagrant-dev-vm'
-    cd ~/Code/sd-chef/cookbooks/sd-vagrant-dev
+    cd ~/Code/vagrant-dev-vm/sd-vagrant-dev
     berks install
     berks update
-    berks package ~/Code/vagrant-dev-vm/sd-vagrant-dev.tar
-    cd ~/Code/vagrant-dev-vm/
-    rm -rf cookbooks
-    tar -xvf sd-vagrant-dev.tar
-    rm sd-vagrant-dev.tar
+    berks vendor ../cookbooks
 end
