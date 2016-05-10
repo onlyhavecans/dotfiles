6917e519284f7257e2c6fc62d135512b9d6324cb
David Aronsohn <WagThatTail@Me.com>
2016-05-10 14:08:09 -0700

    Doctor last, and vundle me

--- fish/functions/pour.fish
+++ fish/functions/pour.fish
@@ -1,7 +1,8 @@
 function pour --description "Update all my homebrew stuff that isn't pinned"
   set -lx PATH /usr/local/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin
-  brew doctor
   brew update
   brew upgrade --cleanup --all
   brew cleanup -s
+  brew doctor
+  vim +PluginUpdate +qall
 end
