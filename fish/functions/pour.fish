80664e31daee64baeeec3e7915dc8d4c2ab37e51
David Aronsohn <WagThatTail@Me.com>
2016-07-31 12:39:33 -0700

    Minor tweaks to commands

--- fish/functions/pour.fish
+++ fish/functions/pour.fish
@@ -3,6 +3,8 @@ function pour --description "Update all my homebrew stuff that isn't pinned"
   brew update
   brew upgrade --cleanup
   brew cleanup -s
+  ## This is because of chef-dk always making broken synlinks
+  brew prune
   brew doctor
   vim +PluginUpdate +qall
 end
