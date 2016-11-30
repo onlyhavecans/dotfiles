c1f02a5521e66a72e4819df50e2c463ec0f6e1fc
David Aronsohn <WagThatTail@Me.com>
2016-11-30 15:22:59 -0800

    Another nvim reference

--- fish/functions/pour.fish
+++ fish/functions/pour.fish
@@ -7,6 +7,6 @@ function pour --description "Update all my homebrew stuff that isn't pinned"
   ## This is because of chef-dk always making broken synlinks
   brew prune
   brew doctor
-  nvim +PlugUpgrade +qall
-  nvim +PlugUpdate
+  vim +PlugUpgrade +qall
+  vim +PlugUpdate
 end
