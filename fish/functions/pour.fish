b970afd25ec30cb0e4be9d39119404573eb6d26f
David Aronsohn <WagThatTail@Me.com>
2017-03-10 13:27:26 -0800

    VIm up

--- fish/functions/pour.fish
+++ fish/functions/pour.fish
@@ -3,8 +3,6 @@ function pour --description "Update all my homebrew stuff that isn't pinned"
   set -lx PATH /usr/local/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin
   brew update
   brew upgrade
-  ## Only clean up older than 60 days because neovim jmalloc issue screwed me
-  brew cleanup --prune=60
   ## This is because of chef-dk always making broken synlinks
   brew prune
   brew doctor
