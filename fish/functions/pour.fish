a92e8b27f8829ab17c8a745bc5560e073a6833e2
David Aronsohn <WagThatTail@Me.com>
2017-04-14 17:23:04 -0700

    Fix annoying chef-dk pregenratation problem

--- fish/functions/pour.fish
+++ fish/functions/pour.fish
@@ -3,17 +3,21 @@ function pour --description "Update all my homebrew stuff that isn't pinned"
   set -lx PATH /usr/local/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin
   brew update
   brew upgrade
-  ## This is because of chef-dk always making broken synlinks
+  status_message brew prune since chef-dk always makes broken synlinks
   brew prune
-  # pregenerate fuck command to save shell launch time
+  status_message pregenerate fuck command to save shell launch time
   if test -x /usr/local/bin/thefuck
     thefuck --alias | source
     funcsave fuck
   end
-  # pregenerate chef-dk to save shell launch timeo
+  status_message pregenerate chef-dk to save shell launch time
   if test -x /opt/chefdk/bin/chef
     /opt/chefdk/bin/chef shell-init fish > {$HOME}/.chefdk.fish
+    set -lx FIND '"/usr/local/bin" "/usr/local/sbin" "/bin" "/sbin" "/usr/bin" "/usr/sbin"'
+    set -lx REPLACE '$PATH'
+    ruby -p -i -e "gsub(ENV['FIND'], ENV['REPLACE'])" {$HOME}/.chefdk.fish
   end
+  status_message doctor brew
   brew doctor
   vim +PlugUpgrade +qall
   vim +PlugUpdate
