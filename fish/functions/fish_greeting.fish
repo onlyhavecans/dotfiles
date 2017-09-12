71071265856492ed39bc97a820d8513f78f354c1
David Aronsohn <WagThatTail@Me.com>
2017-09-12 13:20:15 -0700

    Plenty of minor tweaks

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,11 +1,9 @@
 function fish_greeting
-  if test -z $VSCODE_PID # Not in VSCode
-    if test -x "/usr/local/bin/ponysay"
-      /usr/local/bin/ponysay --restrict NAME=Rarity --ponyonly
-    else if test -x "/usr/local/bin/archey"
-      /usr/local/bin/archey --color --offline --packager
-    else if test -x "/usr/local/bin/bsdinfo"
-      /usr/local/bin/bsdinfo
-    end
+  if test -x "/usr/local/bin/ponysay"
+    /usr/local/bin/ponysay --restrict NAME=Rarity --ponyonly
+  else if test -x "/usr/local/bin/archey"
+    /usr/local/bin/archey --color --offline --packager
+  else if test -x "/usr/local/bin/bsdinfo"
+    /usr/local/bin/bsdinfo
   end
 end
