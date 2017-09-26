09c7ac89f8503ea3eb0d6ea1911b7189e6728116
David Aronsohn <WagThatTail@Me.com>
2017-09-26 12:04:57 -0700

    Clean up pour

--- fish/functions/pour.fish
+++ fish/functions/pour.fish
@@ -3,21 +3,37 @@ function pour --description "Update all my homebrew stuff that isn't pinned"
   set -lx PATH /usr/local/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin $HOME/.cargo/bin
   brew update
   brew upgrade
+
   status_message brew prune since chef-dk always makes broken synlinks
   brew prune
-  status_message pregenerate fuck command to save shell launch time
+
   if test -x /usr/local/bin/thefuck
+    status_message pregenerate fuck command to save shell launch time
     thefuck --alias | source
     funcsave fuck
   end
-  status_message Rust it up
-  rustup update
+
+  if test -x $HOME/.cargo/bin/rustup
+    status_message Rust it up
+    rustup update
+  end
+
+  status_message update all the vim stuff
+  vim +PlugUpgrade +qall
+  vim +PlugUpdate +qall
+
+  if test -x $HOME/.tmux/plugins/tpm/bin/update_plugins
+    status_message update tmux plugins
+    ~/.tmux/plugins/tpm/bin/update_plugins all
+  end
+
   status_message doctor brew
   brew doctor
+  status_message Check for missing packages
   brew missing
+
   if test -x /usr/local/bin/mas
+    status_message do I have any outdated App Store apps
     mas outdated
   end
-  vim +PlugUpgrade +qall
-  vim +PlugUpdate
 end
