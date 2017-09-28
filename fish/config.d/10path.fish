12d8f6e9e36d64f4dec56a88207950aeac566380
David Aronsohn <WagThatTail@Me.com>
2017-09-28 12:25:41 -0700

    Move our PG.app path

--- fish/config.d/10path.fish
+++ fish/config.d/10path.fish
@@ -1,13 +1,14 @@
 ## I like being strict with my path in shells
 if status is-interactive
   set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
+  set -Ue fish_user_paths
 
   set -l prepend_dirs $HOME/bin $HOME/Applications
   for dir in $prepend_dirs
     test -d $dir; and set -x PATH $dir $PATH
   end
 
-  set -l append_dirs $HOME/.fzf/bin /opt/X11/bin /Applications/Postgres.app/Contents/Versions/latest/bin
+  set -l append_dirs $HOME/.fzf/bin /opt/X11/bin
   for dir in $append_dirs
     test -d $dir; and set -x PATH $PATH $dir
   end
---
+++ fish/config.d/20pgapp.fish
@@ -0,0 +1,4 @@
+set -l pg_app /Applications/Postgres.app/Contents/Versions/latest/bin
+if test -d $pg_app
+  append_path %pg_app
+end
