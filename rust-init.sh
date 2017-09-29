b06b0193715d918a91301de6cce06aae1335ba79
David Aronsohn <WagThatTail@Me.com>
2017-09-29 12:04:28 -0700

    update rust

--- rust-init.sh
+++ rust-init.sh
@@ -6,15 +6,16 @@ echo "This isn't a regular run script, it's gonna force reinstal crates"
 rustup self update
 rustup update
 
+rustup install stable
 rustup install nightly
 rustup default nightly
 
 rustup target add x86_64-unknown-freebsd
 rustup target add x86_64-unknown-linux-gnu
 
-rustup component add rust-src
-rustup component add rust-analysis
-rustup component add rls
+rustup component add rls --toolchain nightly
+rustup component add rust-analysis --toolchain nightly
+rustup component add rust-src --toolchain nightly
 
 rustup run nightly cargo install rustfmt-nightly --force
 rustup run nightly cargo install clippy --force
