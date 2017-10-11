775f41bbea707f87b42903bcf2bb380de9d5d461
David Aronsohn <WagThatTail@Me.com>
2017-10-11 15:02:19 +0100

    Fix metadata

---
+++ cookbooks/workstation/metadata.rb
@@ -0,0 +1,13 @@
+name             'workstation'
+maintainer       'tBunnyMan'
+maintainer_email 'WagThatTail@Me.com'
+issues_url       'https://github.com/onlyhavecans/dotfiles/issues'
+source_url       'https://github.com/onlyhavecans/dotfiles'
+license          'BSD-3-Clause'
+version          '8.0.0'
+
+chef_version '>= 12.7'
+
+supports 'mac_os_x', 'freebsd'
+
+depends 'homebrew'
