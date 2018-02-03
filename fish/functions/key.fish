21be3c57e0a0fbb5dc2227deaf80134348e70b03
David Aronsohn <WagThatTail@Me.com>
2018-02-02 17:48:47 -0800

    clean uyp key making
    Signed-off-by: David Aronsohn <WagThatTail@Me.com>

--- fish/functions/key.fish
+++ fish/functions/key.fish
@@ -1,4 +1,4 @@
-function key --description "Load my key for the year and that machine"
+function key --description "Load my key for the year"
   ssh-add -t 12h ~/.ssh/*-(date +%Y)_rsa
   ssh-add -t 12h ~/.ssh/*-(date +%Y)_ed25519
 end
--- fish/functions/keygen.fish
+++ fish/functions/keygen.fish
@@ -1,4 +1,4 @@
 function keygen --description "How I am generating keys currently"
-  ssh-keygen -t ed25519 -C dos@(hostname)-(date -v+1y +%Y) -f ~/.ssh/dos@(hostname)-(date -v+1y +%Y)_ed25519
-  ssh-keygen -t rsa -b 4096 -C dos@(hostname)-(date -v+1y +%Y) -f ~/.ssh/dos@(hostname)-(date -v+1y +%Y)_rsa
+  ssh-keygen -t ed25519 -C (whoami)@(hostname)-(date +%F) -f ~/.ssh/(whoami)@(hostname)-(date +%Y)_ed25519
+  ssh-keygen -t rsa -b 4096 -C (whoami)@(hostname)-(date +%F) -f ~/.ssh/(whoami)@(hostname)-(date +%Y)_rsa
 end
