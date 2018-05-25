35c331c8726359fd005315f32b9706cb013852cb
David Aronsohn <WagThatTail@Me.com>
2018-05-25 12:10:43 -0500

    PR GNUS
    Signed-off-by: David Aronsohn <WagThatTail@Me.com>

--- fish/functions/gpr.fish
+++ fish/functions/gpr.fish
@@ -1,5 +1,7 @@
 function gpr --description "Push (to my fork if present) and open PR"
+
   set -l my_fork "onlyhavecans"
+
   if git remote | grep --silent $my_fork
     status_message "Personal fork present, pushing to $my_fork"
     git push $my_fork
@@ -7,6 +9,8 @@ function gpr --description "Push (to my fork if present) and open PR"
     status_message "no fork present, pushing to origin"
     git push origin
   end
+
+  afplay (random choice (find ~/ResilioSync/Documents/PR-GNUS -iname \*mp3)) &
 
   git pull-request --browse
 end
