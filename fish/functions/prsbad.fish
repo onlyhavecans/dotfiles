474b1945b9557c88caafc036da5fea90f5346400
David Aronsohn <WagThatTail@Me.com>
2018-05-30 17:32:41 -0700

    Support auto-assign for new PRs
    Signed-off-by: David Aronsohn <WagThatTail@Me.com>

--- fish/functions/prsbad.fish
+++ fish/functions/prsbad.fish
@@ -1,16 +1,16 @@
 function prsbad --description "Push (to my fork if present) and open PR while reminding me PR'S BAD"
-  set -l my_fork "onlyhavecans"
+  set -l my_github_user "onlyhavecans"
   set -l audio_file (random choice (find ~/ResilioSync/Documents/PR-GNUS -iname \*mp3))
 
   afplay $audio_file &
 
-  if git remote | grep --silent $my_fork
-    status_message "Personal fork present, pushing to $my_fork"
-    git push $my_fork
+  if git remote | grep --silent $my_github_user
+    status_message "Personal fork present, pushing to $my_github_user"
+    git push $my_github_user
   else
     status_message "no fork present, pushing to origin"
     git push origin
   end
 
-  hub pull-request --browse
+  hub pull-request --assign=$my_github_user --push --browse
 end
