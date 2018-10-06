e67907595094c9dc4acac2db5969f3b9e728bb1f
Amy Aronsohn <WagThatTail@Me.com>
2018-10-05 18:13:38 -0700

    No more global system key
    Signed-off-by: Amy Aronsohn <WagThatTail@Me.com>

--- fish/functions/key.fish
+++
@@ -1,4 +0,0 @@
-function key --description "Load my key for the year"
-  ssh-add -t 12h ~/.ssh/*-(date +%Y)_rsa
-  ssh-add -t 12h ~/.ssh/*-(date +%Y)_ed25519
-end
--- fish/functions/keygen.fish
+++ fish/functions/keygen.fish
@@ -1,4 +1,10 @@
 function keygen --description "How I am generating keys currently"
-  ssh-keygen -t ed25519 -C (whoami)@(hostname)-(date +%F) -f ~/.ssh/(whoami)@(hostname)-(date +%Y)_ed25519
-  ssh-keygen -t rsa -b 4096 -C (whoami)@(hostname)-(date +%F) -f ~/.ssh/(whoami)@(hostname)-(date +%Y)_rsa
+  status_message "What hostname is this key for?"
+  read -l key_remote
+
+  set -l key_dir "$HOME/.ssh/keys/$key_remote"
+  mkdir -p "$key_dir"
+
+  ssh-keygen -t ed25519 -C (whoami)@(hostname)-(date +%F) -f "$key_dir/id_ed25519"
+  ssh-keygen -t rsa -b 4096 -C (whoami)@(hostname)-(date +%F) -f "$key_dir/id_rsa"
 end
