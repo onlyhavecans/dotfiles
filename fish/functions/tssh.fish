21c22bb4fc0973ff47cd272b215a8dff58988024
Amy Aronsohn <WagThatTail@Me.com>
2018-10-11 21:11:59 -0700

    Fix all my accidental usage
    Signed-off-by: Amy Aronsohn <WagThatTail@Me.com>

--- fish/functions/tssh.fish
+++ fish/functions/tssh.fish
@@ -1,4 +1,8 @@
 function tssh --description "Set term for tmux ssh sessions"
-  set -lx TERM xterm-256color
-  ssh colo01.squirrels.wtf
+  if test 0 -ne (count $argv)
+    status_message "That's not what this is for anymore"
+  else
+    set -lx TERM xterm-256color
+    ssh colo01.squirrels.wtf
+  end
 end
