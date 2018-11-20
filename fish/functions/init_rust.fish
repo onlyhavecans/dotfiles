f3592daead17ad8255a02789b163bb9ec2259f82
Amy Aronsohn <WagThatTail@Me.com>
2018-11-20 11:28:41 -0800

    bring in all the rust for testing
    Signed-off-by: Amy Aronsohn <WagThatTail@Me.com>

--- fish/functions/init_rust.fish
+++ fish/functions/init_rust.fish
@@ -6,14 +6,16 @@ function init_rust
   rustup self update
   rustup update
 
-  rustup install $MY_TOOLCHAIN
+  for MY_TOOLCHAIN in stable beta nightly
+    rustup install $MY_TOOLCHAIN
 
-  rustup default $MY_TOOLCHAIN
+    rustup target add x86_64-unknown-freebsd --toolchain $MY_TOOLCHAIN
+    rustup target add x86_64-unknown-linux-gnu --toolchain $MY_TOOLCHAIN
 
-  rustup target add x86_64-unknown-freebsd
-  rustup target add x86_64-unknown-linux-gnu
-
-  for component in rustfmt-preview rls-preview rust-analysis rust-src
-    rustup component add $component --toolchain $MY_TOOLCHAIN
+    for component in clippy-preview rustfmt-preview rls-preview rust-analysis rust-src
+      rustup component add $component --toolchain $MY_TOOLCHAIN
+    end
   end
+
+  rustup default stable
 end
