518bf3b646437ebb88489d1ede3d91de9b5f76eb
Amy Aronsohn <WagThatTail@Me.com>
2018-12-13 13:27:08 -0800

    Add my neovim pyenvs to pour
    Signed-off-by: Amy Aronsohn <WagThatTail@Me.com>

--- fish/functions/pour.fish
+++ fish/functions/pour.fish
@@ -1,6 +1,6 @@
 function pour --description "Update all my homebrew stuff that isn't pinned"
   ## Don't look at all the chef-dk stuff
-  set -lx PATH /usr/local/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin $HOME/.cargo/bin
+  set -lx PATH $HOME/.pyenv/shims $HOME/.cargo/bin /usr/local/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin
   brew update
   brew upgrade
 
@@ -8,7 +8,7 @@ function pour --description "Update all my homebrew stuff that isn't pinned"
   brew prune
 
   if test -x /usr/local/bin/thefuck
-    status_message pregenerate fuck command to save shell launch time
+    status_message Pregenerate fuck command to save shell launch time
     thefuck --alias | source
     funcsave fuck
   end
@@ -17,6 +17,26 @@ function pour --description "Update all my homebrew stuff that isn't pinned"
     status_message Rust update
     rustup update
   end
+
+  status_message Set up and update neovim python
+  status_message Set up python 2 neovim
+  if not test -x $HOME/.pyenv/versions/neovim2
+    pyenv virtualenv 2.7.15 neovim2
+  end
+
+  set -lx PYENV_VERSION neovim2
+  pip install --upgrade pip
+  pip install --upgrade neovim 'python-language-server[all]' autopep8 rope flake8 pylint pytest
+  set -le PYENV_VERSION
+
+  status_message Set up python 3 neovim
+  if not test -x $HOME/.pyenv/versions/neovim3
+    pyenv virtualenv 3.7.1 neovim3
+  end
+  set -lx PYENV_VERSION neovim3
+  pip install --upgrade pip
+  pip install -U neovim 'python-language-server[all]' autopep8 rope flake8 pylint pytest
+  set -le PYENV_VERSION
 
   status_message Clean up vim-plug repos to prevent errors
   for plugin in ~/.config/nvim/plugged/*
