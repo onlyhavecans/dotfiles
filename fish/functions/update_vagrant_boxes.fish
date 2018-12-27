24ec1938ef7871cf01329d7555de89f72c91e78e
Amelia Aronsohn <WagThatTail@Me.com>
2018-12-27 11:10:58 -0800

    Use the vagrat provider I set
    Signed-off-by: Amelia Aronsohn <WagThatTail@Me.com>

--- fish/functions/update_vagrant_boxes.fish
+++ fish/functions/update_vagrant_boxes.fish
@@ -1,6 +1,6 @@
 function update_vagrant_boxes --description "Update all my boxes (often just bentos)"
   for box in (vagrant box outdated --global | tr -d "*'" | awk '/outdated/ {print $1}')
-    vagrant box add --clean --provider vmware_desktop $box
+    vagrant box add --clean --provider $VAGRANT_DEFAULT_PROVIDER $box
   end
 
   vagrant box prune --force
