da884b60cde564fb670a657ce6d30b2a682f3f0d
Amelia Aronsohn <WagThatTail@Me.com>
2019-01-04 18:20:11 -0800

    Rotate old keys instead of overwriting
    Signed-off-by: Amelia Aronsohn <WagThatTail@Me.com>

--- fish/functions/keygen.fish
+++ fish/functions/keygen.fish
@@ -5,6 +5,16 @@ function keygen --description "How I am generating keys currently"
   set -l key_dir "$HOME/.ssh/keys/$key_remote"
   mkdir -p "$key_dir"
 
-  ssh-keygen -t ed25519 -C (whoami)@(hostname)-(date +%F) -f "$key_dir/id_ed25519"
-  ssh-keygen -t rsa -b 4096 -C (whoami)@(hostname)-(date +%F) -f "$key_dir/id_rsa"
+  set -l key_rsa "$key_dir/id_rsa"
+  set -l key_ed "$key_dir/id_ed25519"
+
+  for old_key in $key_ed $key_rsa $key_ed.pub $key_rsa.pub
+    if test -f $old_key
+      status_message "Rotating away old key $old_key"
+      mv "$old_key" "$old_key-rotated-"(date +%F)
+    end
+  end
+
+  ssh-keygen -t ed25519 -C (whoami)@(hostname)-(date +%F) -f "$key_ed"
+  ssh-keygen -t rsa -b 4096 -C (whoami)@(hostname)-(date +%F) -f "$key_rsa"
 end
