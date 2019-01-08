59b18e2395c8e5c10f7639499171f7edda44914d
Amelia Aronsohn <WagThatTail@Me.com>
2019-01-08 10:58:43 -0800

    Make dnpull work in dir called
    Signed-off-by: Amelia Aronsohn <WagThatTail@Me.com>

--- fish/functions/dnpull.fish
+++ fish/functions/dnpull.fish
@@ -1,24 +1,22 @@
 function dnpull --description "pull all my cookbooks up up"
-  set -l dnpaths ~/Code ~/go/src/github.com/dnsimple
-  for top_dir in $dnpaths
-    for dir in (fd --hidden --no-ignore --fixed-strings .git --type d --exclude .direnv --exclude gocode --exclude .github $top_dir)
-      cd $dir/..
-      set -l local_dir (pwd)
+  set -l my_directory
+  for dir in (fd --hidden --no-ignore --fixed-strings .git --type d --exclude .direnv --exclude gocode --exclude .github $my_directory)
+    cd $dir/..
+    set -l local_dir (pwd)
 
-      if test (git remote | wc -l) -eq 0
-        status_message $local_dir does not have a remote so skipping
-        continue
-      end
-
-      status_message Updating $local_dir
-      git fetch --quiet
-
-      git checkout master --quiet
-      if test $status != 0
-        continue
-      end
-      git pull
+    if test (git remote | wc -l) -eq 0
+      status_message $local_dir does not have a remote so skipping
+      continue
     end
+
+    status_message Updating $local_dir
+    git fetch --quiet
+
+    git checkout master --quiet
+    if test $status != 0
+      continue
+    end
+    git pull
   end
-  cd ~
+  cd $my_directory
 end
