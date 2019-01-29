ed03012a9cf99830efbef46cd3603f028c0662d8
Amelia Aronsohn <WagThatTail@Me.com>
2019-01-29 12:14:18 -0800

    Have fish figure out vagrant or vmware
    Signed-off-by: Amelia Aronsohn <WagThatTail@Me.com>

--- fish/config.d/30vagrant.fish
+++ fish/config.d/30vagrant.fish
@@ -1,3 +1,7 @@
-# I use vmware
-set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
-abbr --add vmrun '/Applications/VMware\ Fusion.app/Contents/Library/vmrun'
+# I sometimes use vmware
+if test -x '/Applications/VMware\ Fusion.app/Contents/Library/vmrun'
+  set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
+  abbr --add vmrun '/Applications/VMware\ Fusion.app/Contents/Library/vmrun'
+else
+  set -x VAGRANT_DEFAULT_PROVIDER virtualbox
+end
