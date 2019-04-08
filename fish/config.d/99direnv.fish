a610bbeb5b7f7315d25afd99a50129b7f2aad2d7
Amelia Aronsohn <WagThatTail@Me.com>
2019-04-08 14:10:19 -0700

    Update direnv load
    Signed-off-by: Amelia Aronsohn <WagThatTail@Me.com>

--- fish/config.d/99direnv.fish
+++ fish/config.d/99direnv.fish
@@ -1,2 +1,4 @@
-alias tmux "direnv exec / tmux"
-test -x /usr/local/bin/direnv; and eval (direnv hook fish)
+if test -x /usr/local/bin/direnv
+  direnv hook fish | source
+  alias tmux "direnv exec / tmux"
+end
