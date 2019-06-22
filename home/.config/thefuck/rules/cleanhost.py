fc6cd506856d8720f15d82043cf30065f8e8e272
Amelia Aronsohn <squirrel@wearing.black>
2019-06-21 19:40:41 -0700

    lint and love thefuck cleanhost, add differs check

--- home/.config/thefuck/rules/cleanhost.py
+++ home/.config/thefuck/rules/cleanhost.py
@@ -1,12 +1,22 @@
+"""
+Shortcut for cleaning hosts that have been replace if I forgot
+"""
+
 from thefuck.utils import for_app
+
 
 @for_app('ssh', at_least=1)
 def match(command):
+    """Match all the SSH errors"""
     return (
-        'WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!' in command.output
+        'WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!' in command.output or
+        'differs from the key for the IP address' in command.output
     )
 
+
 def get_new_command(command):
+    """replace"""
     return command.script.replace('ssh', 'cleanhost', 1)
+
 
 enabled_by_default = True
