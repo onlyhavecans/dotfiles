b5591ec8387c0ab641e57747c117f8de00986efc
Amelia Aronsohn <squirrel@wearing.black>
2019-09-24 11:00:00 -0700

    Reorganize config.fish to optimize
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,12 @@
-#config.d/00preload_functions.fish
+###
+## Start with Functions
+###
+
 function prepend_path
   if not test 1 -eq (count $argv)
     echo "only one path at a time"
     return 1
   end
-
   set -l my_path $argv
   contains -- $my_path $PATH
     or set -x PATH $my_path $PATH
@@ -15,15 +17,16 @@ function append_path
     echo "only one path at a time"
     return 1
   end
-
   set -l my_path $argv
   contains -- $my_path $PATH
     or set -x PATH $PATH $my_path
 end
 
-#config.d/10path.fish
-## I like being strict with my path in shells
-if status is-interactive
+###
+## All the stuff that should only happen if interactive
+###
+
+if status --is-interactive
   set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
   set -Ue fish_user_paths
 
@@ -36,71 +39,33 @@ if status is-interactive
   for dir in $append_dirs
     test -d $dir; and set -x PATH $PATH $dir
   end
-end
 
-#config.d/20homeshick.fish
-source "$HOME/.homesick/repos/homeshick/homeshick.fish"
-source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
-homeshick refresh --quiet --batch
+  # Homeshick is important
+  source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+  source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
+  homeshick refresh --quiet --batch
 
-#config.d/20pgapp.fish
-set -l pg_app /Applications/Postgres.app/Contents/Versions/latest/bin
-if test -d $pg_app
-  append_path $pg_app
-end
+  # I am lazy with neovim
+  alias vim nvim
+  set -x EDITOR nvim
 
-## Some rust
-set -l rustup_path $HOME/.cargo/bin
-if test -n $CARGO_HOME
-  set -l rustup_path $CARGO_HOME/bin
-end
+  # Fix tmux term
+  if test -z "$TMUX"
+    set TERM screen-256color
+  end
 
-append_path $rustup_path
-set -x RUST_SRC_PATH $HOME/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src
-
-#config.d/25go.fish
-## go for golang
-set -l go_path $HOME/go
-set -l go_bin $go_path/bin
-
-append_path $go_bin
-
-#config.d/30editor.fish
-alias vim nvim
-set -x EDITOR nvim
-
-#config.d/30term.fish
-if test -z "$TMUX"
-  set TERM screen-256color
-end
-
-#config.d/30vagrant.fish
-# I sometimes use vmware
-if test -x '/Applications/VMware\ Fusion.app/Contents/Library/vmrun'
-  set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
-else
-  set -x VAGRANT_DEFAULT_PROVIDER virtualbox
-end
-
-#config.d/40chef.fish
-if status --is-interactive
+  # Chef is important
   abbr --add --global ce chef exec
   abbr --add --global cebu chef exec berks update
-end
 
-# fzf in fish
-set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --no-ignore-vcs'
-if status --is-interactive
+  # fzf in fish
   alias c "cd (fd . --full-path ~ --type d --no-ignore-vcs| fzf)"
   alias v "nvim (fzf)"
-end
 
-#config.d/40exa.fishj
-alias ls exa
+  # I prefer Exa
+  alias ls exa
 
-#config.d/40git.fish
-## Mo Git Mo Problems
-if status --is-interactive
+  # Mo Git Mo Problems
   alias git hub
   alias g hub
   abbr --add --global gad git add --all
@@ -112,15 +77,53 @@ if status --is-interactive
   abbr --add --global gst git status
   abbr --add --global gft git fetch --tags
   abbr --add --global gbr git browse
+
+  # Make iTerm Happy
+  test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+  # Quick Mosh alias
+  abbr --add --global cleanmosh killall -USR1 mosh-server
+
+end ## The end of interactive only
+
+###
+## GLobals & Non-Interactive Settings
+###
+
+# I like PRG AP
+set -l pg_app /Applications/Postgres.app/Contents/Versions/latest/bin
+if test -d $pg_app
+  append_path $pg_app
 end
 
-#config.d/40gpg.fish
+# Get Rusty
+set -l rustup_path $HOME/.cargo/bin
+if test -n $CARGO_HOME
+  set -l rustup_path $CARGO_HOME/bin
+end
+append_path $rustup_path
+set -x RUST_SRC_PATH $HOME/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src
+
+# go for golang
+set -l go_path $HOME/go
+set -l go_bin $go_path/bin
+append_path $go_bin
+
+
+# I sometimes use vmware
+if test -x '/Applications/VMware\ Fusion.app/Contents/Library/vmrun'
+  set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
+else
+  set -x VAGRANT_DEFAULT_PROVIDER virtualbox
+end
+
+# This hotfix makes GPG less annoying
 set -x GPG_TTY (tty)
 
-#config.d/90iterm.fish
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+# Global FZF setting
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --no-ignore-vcs'
 
-#config.d/99direnv.fish
+# Direnv makes life easier but needs az tmux fix
 if test -x /usr/local/bin/direnv
   direnv hook fish | source
   alias tmux "direnv exec / tmux"
@@ -131,12 +134,11 @@ end
 set -x MOSH_SERVER_SIGNAL_TMOUT 60
 # Clean up any session that has not been connected to in 30 days
 set -x MOSH_SERVER_NETWORK_TMOUT 2592000
-if status --is-interactive
-  abbr --add --global cleanmosh killall -USR1 mosh-server
-end
 
-## ASDF manager
+# ASDF Makes polyglots happy
 test -e /usr/local/opt/asdf/asdf.fish; and source /usr/local/opt/asdf/asdf.fish
 
-#config.d/99local.fish
+###
+## Sauce Local
+###
 test -e {$HOME}/.config/local/config.fish; and source {$HOME}/.config/local/config.fish
