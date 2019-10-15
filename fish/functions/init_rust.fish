168263d00ed9025db296e7a4d3cfec54eff3368f
Amelia Aronsohn <squirrel@wearing.black>
2019-10-15 11:32:39 -0700

    Switch to using profiles
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- fish/functions/init_rust.fish
+++ fish/functions/init_rust.fish
@@ -5,16 +5,13 @@ function init_rust
 
   rustup self update
   rustup update
+  rustup set profile minimal
 
   for MY_TOOLCHAIN in stable beta nightly
     rustup install $MY_TOOLCHAIN
 
     rustup target add x86_64-unknown-freebsd --toolchain $MY_TOOLCHAIN
     rustup target add x86_64-unknown-linux-gnu --toolchain $MY_TOOLCHAIN
-
-    for component in clippy-preview rustfmt-preview rls-preview rust-analysis rust-src
-      rustup component add $component --toolchain $MY_TOOLCHAIN
-    end
   end
 
   rustup default stable
