d191257c5b988fd6022a8f0bd75b277e1fd7ed52
Amelia Aronsohn <squirrel@wearing.black>
2020-02-19 10:26:07 -0800

    Add path even outside of interactive
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- fish/config.fish
+++ fish/config.fish
@@ -26,20 +26,20 @@ end
 ## All the stuff that should only happen if interactive
 ###
 
+set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
+set -Ue fish_user_paths
+
+set -l prepend_dirs $HOME/bin $HOME/Applications
+for dir in $prepend_dirs
+  test -d $dir; and set -x PATH $dir $PATH
+end
+
+set -l append_dirs /opt/X11/bin
+for dir in $append_dirs
+  test -d $dir; and set -x PATH $PATH $dir
+end
+
 if status --is-interactive
-  set -x PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin
-  set -Ue fish_user_paths
-
-  set -l prepend_dirs $HOME/bin $HOME/Applications
-  for dir in $prepend_dirs
-    test -d $dir; and set -x PATH $dir $PATH
-  end
-
-  set -l append_dirs /opt/X11/bin
-  for dir in $append_dirs
-    test -d $dir; and set -x PATH $PATH $dir
-  end
-
   # Homeshick is important
   source "$HOME/.homesick/repos/homeshick/homeshick.fish"
   source "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
