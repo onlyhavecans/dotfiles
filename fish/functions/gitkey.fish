3792b218029c59029d70cae3554ee01371266f5f
Amelia Aronsohn <squirrel@wearing.black>
2020-05-26 10:15:13 -0700

    Consolodate all my git commands
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- fish/functions/gitkey.fish
+++
@@ -1,3 +0,0 @@
-function gitkey --description "Load my git key for IntelliJ editors"
-  ssh-add -t 12h ~/.ssh/keys/github.com/git/id_ed25519
-end
--- fish/functions/gitpurge.fish
+++
@@ -1,6 +0,0 @@
-function gitpurge
-  status_message "This deletes all but current and master branches. gitclean is cleaner"
-  for branch in (git branch -vv | awk '{ if( !( $1 == "master" || $1 == "*" ) ) print $1}')
-    git branch -D $branch
-  end
-end
