a44b4db42eb3874e3130ffb25e1da471710d55c3
Amelia Aronsohn <squirrel@wearing.black>
2020-06-22 09:37:16 -0700

    Add a local clone fixer
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

---
+++ fish/functions/fix_git_local_clone.fish
@@ -0,0 +1,8 @@
+function fix_git_branches --description "Fix master usage in git"
+  git checkout master
+  git branch -m master main
+  git fetch -p
+  git branch --unset-upstream
+  git branch -u origin/main
+  git symbolic-ref refs/remotes/origin/HEAD refs/remotes/origin/main
+end
