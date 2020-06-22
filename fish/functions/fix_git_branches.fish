f8c936f9876af48daf90d3248c52da61f1c39b57
Amelia Aronsohn <squirrel@wearing.black>
2020-06-22 09:40:09 -0700

    Need quotes
    Signed-off-by: Amelia Aronsohn <squirrel@wearing.black>

--- fish/functions/fix_git_branches.fish
+++ fish/functions/fix_git_branches.fish
@@ -4,6 +4,6 @@ function fix_git_branches --description "Fix master usage in git"
   git branch -m master main
   git push -u origin main
 
-  user_prompt Please Update Default branch in Github/Gitea before continuing
+  user_prompt "Please Update Default branch in Github/Gitea before continuing"
   git push orgin --delete master
 end
