60432cb483689daec5ca43f7bc8eb67e5d1224a8
Amelia Aronsohn <squirrel@wearing.black>
2020-10-09 17:59:11 -0700

    tweaks and bugfixes

--- fish/functions/bujou-dayone.fish
+++ fish/functions/bujou-dayone.fish
@@ -1,6 +1,6 @@
-function bujo-dayone --description "Put Bujo pages into dayone"
+function bujou-dayone --description "Put Bujou pages into dayone"
 	for i in *.png
 		set -l d (string match --regex '\d+-\d+-\d+' $i)
-		dayone2 --tags BuJouPage --date "$d 23:00" --photos $i -- new BouJo Page
+		dayone2 --tags BuJouPage --date "$d 23:00" --photos $i -- new BuJou Page
 	end
 end
