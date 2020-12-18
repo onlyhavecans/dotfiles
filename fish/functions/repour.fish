699e0014a187b91ccf5fb2c0939ee69ef9772d17
Amelia Aronsohn <squirrel@wearing.black>
2020-12-18 12:18:42 -0800

    Update deprecation and problematic commands

--- fish/functions/repour.fish
+++ fish/functions/repour.fish
@@ -1,13 +1,11 @@
 function repour --description "recompile all of brew"
   set -lx PATH /usr/local/bin /usr/local/sbin /bin /sbin /usr/bin /usr/sbin
   xcode-select --install
-  sudo xcodebuild -license accept
   brew update
   brew bundle install --global
   brew upgrade
   for count in 1 2
-    brew list | xargs brew reinstall
+    brew list --formula | xargs brew reinstall
   end
   brew cleanup -s
-  brew bundle cleanup --global
 end
