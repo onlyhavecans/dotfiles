5247d3c674f51d8de9bf8a255c85191bc114efc9
Amelia Aronsohn <dos@Madison.local>
2024-11-22 22:02:26 -0800

    refactor: organize more into conf

--- home/.config/fish/functions/fixssh.fish
+++
@@ -1,3 +0,0 @@
-function fixssh --description 'Update SSH_AUTH_SOCK from parent window to fix SSH keys'
-    set -x SSH_AUTH_SOCK (tmux showenv | grep "SSH_AUTH_SOCK" | sed -e "s@SSH_AUTH_SOCK=@@")
-end
--- home/.config/fish/functions/flakify.fish
+++
@@ -1,9 +0,0 @@
-function flakify --description "Init flake+direnv"
-    if ! test -e flake.nix
-        nix flake new -t github:nix-community/nix-direnv .
-    else if ! test -e .envrc
-        echo 'use flake' >.envrc
-        direnv allow .
-    end
-    nvim flake.nix
-end
