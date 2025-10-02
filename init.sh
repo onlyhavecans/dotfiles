e546fd68472cc791b665599f090a5e09aab347ff
Amelia Aronsohn <squirrel@wearing.black>
2025-10-01 22:19:47 -0700

    fix: forge move init

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Do a curlbash to allow me to take over your system
-# curl -sL https://raw.githubusercontent.com/onlyhavecans/dotfiles/main/init.sh | bash
+# curl -sL https://onlyhavecans.works/dots/castle-dotfiles/raw/branch/main/init.sh | bash
 
 ## Make sure we have git
 for app in git curl gcc make; do
@@ -22,8 +22,8 @@ source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 # clone home, then set it to ssh afterwards
 # This means we need our keys before we can make further actions
 if [[ ! -d $HOME/.homesick/repos/dotfiles ]]; then
-  homeshick --batch clone https://github.com/onlyhavecans/dotfiles.git
-  git -C "$HOME/.homesick/repos/dotfiles" remote set-url origin git@github.com:onlyhavecans/dotfiles
+  homeshick --batch clone https://onlyhavecans.works/dots/castle-dotfiles.git
+  git -C "$HOME/.homesick/repos/dotfiles" remote set-url origin ssh://git@onlyhavecans.works:222/dots/castle-dotfiles.git
 fi
 
 # Link everything
