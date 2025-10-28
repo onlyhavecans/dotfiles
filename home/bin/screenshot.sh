927d526fa9aac7659543f8ec99407c90259252b3
Amelia Aronsohn <squirrel@wearing.black>
2025-10-28 09:29:33 -0700

    feat: quick screen recording

--- home/bin/screenshot.sh
+++ home/bin/screenshot.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 grim -g "$(slurp)" - |
   satty --filename - \
+    --save-after-copy \
+    --font-family Noto \
     --output-filename "$HOME/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png"
