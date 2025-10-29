10ae657fb2f165c9b08e5a9539f2a317f1d767b1
Amelia Aronsohn <squirrel@wearing.black>
2025-10-29 12:39:14 -0700

    fix: screenshot path

--- home/bin/screenshot.sh
+++ home/bin/screenshot.sh
@@ -3,4 +3,4 @@ grim -g "$(slurp)" - |
   satty --filename - \
     --save-after-copy \
     --font-family Noto \
-    --output-filename "$HOME/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png"
+    --output-filename "$HOME/Pictures/Screenshots/satty-$(date '+%Y-%m-%d-%H-%M-%S').png"
