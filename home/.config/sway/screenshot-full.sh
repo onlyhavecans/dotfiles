b3eea951038ca37e6b2958eea6d365129f72ee82
Amelia Aronsohn <squirrel@wearing.black>
2026-01-07 20:58:24 -0800

    chore: dms cleanout

--- home/.config/sway/screenshot-full.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-grim - |
-  satty --filename - \
-    --output-filename "$HOME/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png"
--- home/.config/sway/screenshot-select.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-grim -g "$(slurp)" - |
-  satty --filename - \
-    --output-filename "$HOME/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png"
