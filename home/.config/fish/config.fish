6c76922e250160ac5220edc0f8310e7154f96ede
Amelia Aronsohn <squirrel@wearing.black>
2026-01-09 11:30:52 -0800

    style(fish): sort up and clean up config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -37,40 +37,45 @@ set -g __fish_git_prompt_char_upstream_ahead '↑'
 set -g __fish_git_prompt_char_upstream_behind '↓'
 
 # Basic Abbreviations
-abbr -a --position command l eza
-abbr -a --position command la "eza -a"
-abbr -a --position command ll "eza -l"
-abbr -a --position command lla "eza -la"
-abbr -a --position command tree "eza --tree"
-abbr -a --position command cl clear
-abbr -a --position command nv nvim
-abbr -a --position command man batman
-abbr -a --position command jbat "bat -l json"
-abbr -a --position command pretty prettybat
-abbr -a --position command c claude
-abbr -a --position command cc "claude --continue"
-abbr -a --position command hcd "homeshick cd"
-abbr -a --position command htrack "homeshick track"
-abbr -a --position command http xh
-abbr -a --position command https xhs
-abbr -a --position command o "op run --"
-abbr -a --position command plan "terraform plan -out=tfplan | bat"
-abbr -a --position command tf terraform
-abbr -a --position anywhere -- --help '--help 2>&1 | bat --language=help --plain'
+abbr --add --position command l eza
+abbr --add --position command ls eza
+abbr --add --position command la "eza -a"
+abbr --add --position command ll "eza -l"
+abbr --add --position command lla "eza -la"
+abbr --add --position command tree "eza --tree"
+abbr --add --position command cl clear
+abbr --add --position command nv nvim
+abbr --add --position command hcd "homeshick cd"
+abbr --add --position command htrack "homeshick track"
+
+# pretty things
+abbr --add --position anywhere -- --help '--help 2>&1 | bat --language=help --plain'
+abbr --add --position command man batman
+abbr --add --position command jbat "bat -l json"
+abbr --add --position command pretty prettybat
 
 # Git abbreviations
-abbr -a --position command g git
-abbr -a --position command lg lazygit
-abbr -a --position command gcm --set-cursor 'git commit -m "%"'
-abbr -a --position command gca --set-cursor 'git commit -am "%"'
-abbr -a --position command grs "git remote set-url origin"
-abbr -a --position command gv "lazygit --path=$HOME/Documents/Obsidian/Vault/"
-abbr -a --position command lzd lazydocker
+abbr --add --position command g git
+abbr --add --position command lg lazygit
+abbr --add --position command gcm --set-cursor 'git commit -m "%"'
+abbr --add --position command gca --set-cursor 'git commit -am "%"'
+abbr --add --position command grs "git remote set-url origin"
+abbr --add --position command gv "lazygit --path=$HOME/Documents/Obsidian/Vault/"
+abbr --add --position command lzd lazydocker
+
+# workstuff
+abbr --add --position command c claude
+abbr --add --position command cc "claude --continue"
+abbr --add --position command http xh
+abbr --add --position command https xhs
+abbr --add --position command o "op run --"
+abbr --add --position command plan "terraform plan -out=tfplan | bat"
+abbr --add --position command tf terraform
 
 # Shell In Abbreviations
-abbr -a --position command tm "direnv exec / tmux new-session -A -c ~"
-abbr -a --position command tp "mosh piper.bunni.biz -- tmux new-session -A -c ~"
-abbr -a --position command tw "mosh webby.bunni.biz -- tmux new-session -A -c ~"
+abbr --add --position command tm "direnv exec / tmux new-session -A -c ~"
+abbr --add --position command tp "mosh piper.bunni.biz -- tmux new-session -A -c ~"
+abbr --add --position command tw "mosh webby.bunni.biz -- tmux new-session -A -c ~"
 
 # Tool init
 if type -q fzf
