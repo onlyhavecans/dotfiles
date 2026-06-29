cc1b9dcf843030cd59e36b8c7eb91ef5ed808fec
Amelia Aronsohn <squirrel@wearing.black>
2026-06-29 10:37:03 -0700

    feat(mac): add orbstack cli

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -124,3 +124,7 @@ test -f ~/.homesick/repos/homeshick/homeshick.fish; and source ~/.homesick/repos
 if test -z "$TMUX"
     type -q fastfetch; and fastfetch
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
